4f5be259a0c4de17b30f8aa97cdd149210c4c0f8
Isidro Martín <imarrom@g.educaand.es>
2024-11-20 09:50:36 +0100

    Clase NumerosAleatorios

---
+++ Act3GIT_IsidroMartín/src/numerosAleatorios/NumerosAleatorios.java
@@ -0,0 +1,23 @@
+package numerosAleatorios;
+
+public class NumerosAleatorios {
+
+	private static int MAX		= 10;
+	private static int MIN 		= 1;
+	private static int CANTIDAD	= 20;
+	private static int VALORES	= MAX - MIN + 1;
+	
+	public static void main(String[] args) {
+		double rango	= 0;
+		double numero	= 0;
+		
+		System.out.println("Numeros aleatorios entre " + MIN + " y " + MAX);
+		System.out.println("-------------------------------");
+		for(int i = 0; i < CANTIDAD; i++) {
+			rango = Math.random();
+			numero = rango * VALORES + MIN;
+			System.out.println("Número " + (i+1) + ": " + (int) numero);
+		}
+	}
+
+}
